e0c74eebc5af91fbe5cac56b708bcc3eae5692e3
Runi Brockie <RuniBrockie@live.com>
2016-09-27 18:12:44 +0100

    * Created RESTful API to create, modify and delete contacts using MongoDB

---
+++ app/build/config/app.config.js
@@ -0,0 +1,21 @@
+var mainModule = angular.module('MyApp', ['ngRoute', 'ui.bootstrap'])
+.run(function($rootScope) {
+    
+});
+
+mainModule.config(['$routeProvider',
+  function ($routeProvider) {
+        $routeProvider
+        .when('/home', {
+            templateUrl: 'app/build/templates/home.tpl.html',
+            controller: 'HomeCtrl'
+            /*redirectTo: '/30/dashboard'*/
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+  }]);
+
+mainModule.config(function () {
+
+});
